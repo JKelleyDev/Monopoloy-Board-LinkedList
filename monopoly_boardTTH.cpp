5bb72a27f7152b33ef4cd9c4514de83ee3845c83
jordankelley123 <jordan.d.kelley1@gmail.com>
2024-09-24 14:02:13 -0700

    Updated the a.out

--- monopoly_boardTTH.cpp
+++ monopoly_boardTTH.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
-
+#include <chrono>
+#include <thread>
+#include <vector>
 using namespace std;
 
 // Data class to store a string and an integer
@@ -69,12 +71,17 @@ public:
         headNode = nullptr;
     }
     
+    void sleepTime(int time)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(time));
+    }
+    
     // Mandatory Tasks
     void insertAtHead(T value) {
         
         Node<T>* newNode = new Node<T>(value);
         
-        if (headNode == nullptr)
+        if (isListEmpty())
         {
             headNode = newNode; // If the list is empty, the newNode becomes the headNode
             newNode -> nextNode = headNode; // The new node points to self, since it is circular and the only node in the list.
@@ -97,7 +104,7 @@ public:
         
         Node<T>* newNode = new Node<T>(value);
         
-        if(headNode == nullptr)
+        if(isListEmpty())
         {
             headNode = newNode;
             newNode -> nextNode = headNode;
@@ -117,14 +124,11 @@ public:
     void insertAtPosition(T value, T position) {
         Node<T>* newNode = new Node<T>(value);
         
-        if( headNode == nullptr)
-        {
-            headNode = newNode;
-            newNode -> nextNode = headNode;
-            
+        if(isListEmpty()){
+            cout << "List is empty" << endl;
+            return;
         }else {
             Node<T>* temp = headNode;
-            
             do {
                 if((temp -> data).isEqual(position))
                 {
@@ -135,15 +139,14 @@ public:
                 }
                 temp = temp -> nextNode;
             } while (temp != headNode);
-            
             cout<< "Position value not found. Insertion failed" <<endl;
         }
     }
         
         void deleteAtHead() {
             
-            if(headNode == nullptr){
-                cout << " The list is empty, cannot delete at head." << endl;
+            if(isListEmpty()){
+                cout << "List is empty" << endl;
                 return;
             }
             
@@ -173,8 +176,8 @@ public:
         
         void deleteAtTail() {
             
-            if(headNode == nullptr){
-                cout << " The list is empty, cannot delete at tail." << endl;
+            if(isListEmpty()){
+                cout << "List is empty" << endl;
                 return;
             }
             
@@ -248,8 +251,7 @@ public:
         void printList() {
             Node<T>* temp = headNode;
             int counter = 0;
-            if(headNode == nullptr)
-            {
+            if(isListEmpty()){
                 cout << "List is empty" << endl;
                 return;
             }
@@ -260,6 +262,7 @@ public:
                 cout << "------------------------------" << endl;
                 temp = temp -> nextNode;
                 counter++;
+                sleepTime(300);
             } while (temp != headNode);
         }
         
@@ -273,39 +276,103 @@ public:
             cout << "Print Head Node unwritten" << endl;
         } void printLastNode() {
             cout << "Print Last Node unwritten" << endl;
-        } void isListEmpty() {
-            cout << "Is List Empty unwritten" << endl;
         }
-        void countNodes() {
-            cout << "Count Nodes unwritten" << endl;
+    
+        bool isListEmpty() {
+            return(headNode == nullptr);
+        }
+    
+        int countNodes() {
+            Node<T>* temp = headNode;
+            int counter = 0;
+            
+            if(isListEmpty()){
+                return 0;
+            }
+            do{
+                counter++;
+                temp = temp -> nextNode;
+            }while(temp != headNode);
+            return counter;
         }
         
         //Optional Tasks
         // Advanced Functions
         void convertCLList() {
             cout << "Convert Circular List Unwritten." << endl;
-        } void updateNodeValue() {
-            cout << "update Node value unwritten" << endl;
-        } void displaySpecificColorNode() {
+        }
+    
+        void updateNodeValue(T objectValue, T newValue) {
+            Node<T>* temp = search(objectValue);
+            if(temp){
+                temp -> data = newValue;
+            }else{
+                cout << "Node to be changed not found" << endl;
+            }
+        }
+    
+        void displaySpecificColorNode() {
             cout << "Display Specific color Node" << endl;
         } void mergeCLList() {
             cout << "Merge Circular Linked List Unwritten" << endl;
         }
         
     };
-    
+
+    void sleepTime(int time)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(time));
+    }
+
     // Main function to demonstrate the LinkedList class
     int main() {
         // Create a LinkedList of Data objects
         CircularLinkedList<MonopolyBoard> list;
         
-        MonopolyBoard* spot1 = new MonopolyBoard("A Street", "White", 1254, 120000);
-        MonopolyBoard* spot2 = new MonopolyBoard("B Street", "Green", 1123, 155000);
+        std::vector<MonopolyBoard> spots;
+
+        spots.push_back(MonopolyBoard("Mediterranean Avenue", "Brown", 30, 2));
+        spots.push_back(MonopolyBoard("Baltic Avenue", "Brown", 30, 4));
+        spots.push_back(MonopolyBoard("St.James Place", "Orange", 90, 14));
+        spots.push_back(MonopolyBoard("Tennessee Avenue", "Orange", 90, 14));
+        spots.push_back(MonopolyBoard("New York Avenue", "Orange", 100, 16));
+        spots.push_back(MonopolyBoard("Oriental Avenue", "Light Blue", 50, 6));
+        spots.push_back(MonopolyBoard("Vermont Avenue", "Light Blue", 50, 6));
+        spots.push_back(MonopolyBoard("Connecticut Avenue", "Light Blue", 60, 8));
+        spots.push_back(MonopolyBoard("Kentucky Avenue", "Red", 110, 18));
+        spots.push_back(MonopolyBoard("Indiana Avenue", "Red", 110, 18));
+        spots.push_back(MonopolyBoard("Illinois Avenue", "Red", 120, 20));
+        spots.push_back(MonopolyBoard("St.Charles Place", "Pink", 70, 10));
+        spots.push_back(MonopolyBoard("States Avenue", "Pink", 70, 10));
+        spots.push_back(MonopolyBoard("Virginia Avenue", "Pink", 80, 12));
+        spots.push_back(MonopolyBoard("Atlantic Avenue", "Yellow", 1123, 155000));
+        spots.push_back(MonopolyBoard("Ventor Avenue", "Yellow", 1123, 155000));
+        spots.push_back(MonopolyBoard("Marvin Gardens", "Yellow", 1123, 155000));
+        spots.push_back(MonopolyBoard("Pacific Avenue", "Green", 1123, 155000));
+        spots.push_back(MonopolyBoard("North Carolina Avenue", "Green", 1123, 155000));
+        spots.push_back(MonopolyBoard("Pennsylvania Avenue", "Green", 1123, 155000));
+        spots.push_back(MonopolyBoard("Park Place", "Dark Blue", 1123, 155000));
+        spots.push_back(MonopolyBoard("Boardwalk", "Dark Blue", 1123, 155000));
+        
+    
+        cout << "Initializing List..." << endl;
+        sleepTime(1200);
+        for(int i = 0; i < spots.size(); i++)
+        {
+            list.insertAtTail(spots[i]);
+            sleepTime(100);
+        }
+
+
+     
+    
+        
+        
         
         // Insert elements at the end
-        list.insertAtHead(*spot1);
+     //   list.insertAtHead();
         
-        list.insertAtTail(*spot2);
+  //      list.insertAtTail();
 
         //list.insertAtPosition();
       
